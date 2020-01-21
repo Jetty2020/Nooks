21b47e294799192a53d73c148b112d37bd8f7dc5
Jetty2020 <59543469+Jetty2020@users.noreply.github.com>
2020-01-21 21:15:47 +0900

    useInput

--- src/index.js
+++ src/index.js
@@ -3,15 +3,30 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = event => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 const App = () => {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const maxLen = value => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
-      <h1>Hello X {item}</h1>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>decrement</button>
+      <h1>Hello </h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 };
