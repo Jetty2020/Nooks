f81d06e7894c5dc7b0c56fd6ff1f3847f26ead0e
Jetty2020 <59543469+Jetty2020@users.noreply.github.com>
2020-01-22 17:26:16 +0900

    useEffect

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-const useTitle = initialTitle => {
-  const [title, setTitle] = useState(initialTitle);
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerText = title;
-  };
-  useEffect(updateTitle, [title]);
-  return setTitle;
-};
-
 const App = () => {
-  const titleUpdater = useTitle("Loading...");
-  setTimeout(() => titleUpdater("Home"), 3000);
+  const sayHello = () => console.log("hello");
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  useEffect(sayHello, [number]);
   return (
     <div className="App">
       <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 };
