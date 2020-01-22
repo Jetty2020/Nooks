e37fd2922434193753488866ddd65e8d9d8258fe
Jetty2020 <59543469+Jetty2020@users.noreply.github.com>
2020-01-22 21:42:57 +0900

    useFadeIn

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
-const useBeforeLeave = onBefore => {
-  if (typeof onBefore !== "function") {
+const useFadeIn = (duration = 1, delay = 0) => {
+  if (typeof duration !== "number" || typeof delay !== "number") {
     return;
   }
-  const handle = () => {
-    onBefore();
-  };
-  /* clientY를 제어하므로 위로 벗어날때만 콘솔로그 되도록 제어
-  const handle = event => {
-    const { clientY } = event;
-    if (clientY <= 0) {
-      onBefore();
-    }
-  };
-      */
+  const element = useRef();
   useEffect(() => {
-    document.addEventListener("mouseleave", handle);
-    return () => document.removeEventListener("mouseleave", handle);
-  });
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  }, []);
+  return { ref: element, style: { opacity: 0 } };
 };
 
 const App = () => {
-  const begForLife = () => console.log("Pls don't leave");
-  useBeforeLeave(begForLife);
+  const fadeInH1 = useFadeIn(3, 2);
+  const fadeInP = useFadeIn(3, 2);
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>asdadw</p>
     </div>
   );
 };
