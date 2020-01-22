b64174a802955010884dcc929d9cabd1d8bf5091
Jetty2020 <59543469+Jetty2020@users.noreply.github.com>
2020-01-22 21:16:20 +0900

    useBeforeLeave

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
-const usePreventLeave = () => {
-  const listener = event => {
-    event.preventDefault();
-    event.returnValue = "";
+const useBeforeLeave = onBefore => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = () => {
+    onBefore();
   };
-  const enablePrevent = () => window.addEventListener("beforeunload", listener);
-  const disablePrevent = () =>
-    window.removeEventListener("beforeunload", listener);
-  return { enablePrevent, disablePrevent };
+  /* clientY를 제어하므로 위로 벗어날때만 콘솔로그 되도록 제어
+  const handle = event => {
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
+  };
+      */
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => document.removeEventListener("mouseleave", handle);
+  });
 };
 
 const App = () => {
-  const { enablePrevent, disablePrevent } = usePreventLeave();
+  const begForLife = () => console.log("Pls don't leave");
+  useBeforeLeave(begForLife);
   return (
     <div className="App">
-      <button onClick={enablePrevent}>Protect</button>
-      <button onClick={disablePrevent}>Unprotect</button>
+      <h1>Hello</h1>
     </div>
   );
 };
